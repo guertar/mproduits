9063184bae6d65a6712d1b2b83a8a11206928ad2
tguerniche <tguerniche@cirb.brussels>
2019-10-13 12:57:16 +0200

    Add config server

--- src/main/java/com/mproduits/web/configuration/ApplicationConfig.java
+++ src/main/java/com/mproduits/web/configuration/ApplicationConfig.java
@@ -1,9 +1,11 @@
 package com.mproduits.web.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
+@RefreshScope
 @ConfigurationProperties("mes-configs")
 public class ApplicationConfig {
 
